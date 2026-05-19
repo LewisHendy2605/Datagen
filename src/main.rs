546074767a077c70012f20b46b932242a15878c6
Lewis Hendy <lewishendy26@gmail.com>
2026-05-19 21:41:54 +0100

    added default args + csv output

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use clap::{Parser, Subcommand};
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io;
+use std::io::Write;
 
 #[derive(Parser)]
+#[command(name = "datagen", about = "Generate fake data files")]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -12,6 +14,7 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Csv {
+        #[arg(default_value = "output.csv")]
         path: String,
 
         #[arg(short, long, default_value_t = 10)]
@@ -19,6 +22,7 @@ enum Commands {
     },
 
     Xml {
+        #[arg(default_value = "output.xml")]
         path: String,
 
         #[arg(short, long, default_value_t = 10)]
@@ -27,15 +31,34 @@ enum Commands {
 }
 
 fn open_file(path: &str) -> Result<File, io::Error> {
-    let res = OpenOptions::new().append(true).create(true).open(path)?;
-    Ok(res)
+    OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(path)
+}
+
+fn write_csv(path: &str, lines: u32) -> Result<(), io::Error> {
+    let mut file = open_file(&path)?;
+
+    let delim = ",";
+
+    // Write Headers
+    write!(file, "index{delim}data\n")?;
+
+    // Write Data
+    for i in 1..=lines {
+        write!(file, "{i}{delim}data\n")?;
+    }
+
+    Ok(())
 }
 
 fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Csv { path, lines: _ } => match open_file(&path) {
+        Commands::Csv { path, lines } => match write_csv(&path, lines) {
             Ok(_file) => println!("Opened successfully"),
             Err(e) => println!("Failed: {e}"),
         },
